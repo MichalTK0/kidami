da6c81c3a6e302d7d3f6edc8c57362d999bef884
MichalTK0 <michalkida2@gmail.com>
2023-10-15 20:55:49 +0100

    Api changes, experience page changes, seperator logic.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from site_app.models import Skill
 
+
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = '__all__'
+        fields = ['title']
