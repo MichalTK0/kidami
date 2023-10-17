c83b9d06ad4d1c63d378e7da0e4d6233d6cc2dca
MichalTK0 <michalkida2@gmail.com>
2023-10-17 20:32:12 +0100

    Test prod setup

--- website/settings.py
+++ website/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+q64)*+pl7+n%+x-x3f)+e+a^)2cty224*vu3i*bpy+3%qczr-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'site_db',
         'ENFORCE_SCHEMA': False,  # Set this to False to use MongoDB as a NoSQL database
         'CLIENT': {
-            'host': 'localhost:27017',
+            'host': os.getenv('DB_HOST'),
         }
     }
 }
